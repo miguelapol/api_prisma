5a3bfbf31a9af8e58ad22cecdfb55dafd7d7af3e
miguelapol <apolonio1995@gmail.com>
2022-05-07 13:20:59 -0500

    se configuro para que aÃse ±adiera a las tabla creacda

--- prisma/seed.js
+++ prisma/seed.js
@@ -3,31 +3,31 @@ const prisma = new PrismaClient();
 
 (async function main() {
   try {
-    const commader = await prisma.explorer.upsert({
+    const commander = await prisma.commander.upsert({
       where: { name: 'Commander' },
       update: {},
       create: {
         name: 'Commander',
 				missionCommander: 'Node',
-        enrollment: 'Woopa'
+        enrollments: 'Woopa'
       },
     });
-    const commander1 = await prisma.explorer.upsert({
+    const commander1 = await prisma.commander.upsert({
       where: { name: 'Commander1' },
       update: {},
       create: {
         name: 'Commander1',
 				missionCommander: 'JavaScript',
-        enrollment: 'Woopa'
+        enrollments: 'Woopa'
       },
     });
-    const Commander2 = await prisma.explorer.upsert({
+    const commander2 = await prisma.commander.upsert({
       where: { name: 'Commander2' },
       update: {},
       create: {
         name: 'Commander2',
 				missionCommander: 'Network',
-        enrollment: 'Woopa'
+        enrollments: 'Woopa'
       },
     });
 
